8a7bb59538214cf1914e2b797e0b6a5b17f4de8d
Josh <joshbcondie@gmail.com>
2014-10-06 13:31:25 -0600

    Transformations on matrices.

--- Transformations/src/Matrix.java
+++ Transformations/src/Matrix.java
@@ -33,7 +33,41 @@ public class Matrix {
 	}
 
 	public void glTranslatef(float x, float y, float z) {
+		Matrix translation = new Matrix();
+		translation.glLoadIdentity();
+		translation.values[3][0] = x;
+		translation.values[3][1] = y;
+		translation.values[3][2] = z;
+		values = Matrix.multiply(translation, this).values;
+	}
 
+	public void glScalef(float x, float y, float z) {
+		Matrix scale = new Matrix();
+		scale.glLoadIdentity();
+		scale.values[0][0] = x;
+		scale.values[1][1] = y;
+		scale.values[2][2] = z;
+		values = Matrix.multiply(scale, this).values;
+	}
+
+	public void glRotateX(float degrees) {
+		Matrix rotation = new Matrix();
+		rotation.glLoadIdentity();
+		rotation.values[1][1] = (float) Math.cos(degrees);
+		rotation.values[2][1] = -(float) Math.sin(degrees);
+		rotation.values[1][2] = (float) Math.sin(degrees);
+		rotation.values[2][2] = (float) Math.cos(degrees);
+		values = Matrix.multiply(rotation, this).values;
+	}
+
+	public void glRotateY(float degrees) {
+		Matrix rotation = new Matrix();
+		rotation.glLoadIdentity();
+		rotation.values[0][0] = (float) Math.cos(degrees);
+		rotation.values[2][0] = (float) Math.sin(degrees);
+		rotation.values[0][2] = -(float) Math.sin(degrees);
+		rotation.values[2][2] = (float) Math.cos(degrees);
+		values = Matrix.multiply(rotation, this).values;
 	}
 
 	public Vector3f toVector3f() {
